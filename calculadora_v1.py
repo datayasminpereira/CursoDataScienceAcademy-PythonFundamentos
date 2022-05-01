fc2cf524a256472d64a119ac97eef7caafdd19ee
Yasmin Pereira <98123224+datayasminpereira@users.noreply.github.com>
2022-05-01 14:55:01 -0300

    cap03_calculadora_by_me

--- calculadora_v1.py
+++ calculadora_v1.py
@@ -6,4 +6,25 @@
 
 print("\n******************* Python Calculator *******************")
 
+print('Selecione o número da operação desejada:')
+print('\n1 - Soma\n2 - Subtração\n3-Multiplicação\n4 - Divisão\n')
+operacao = int(input('Digite sua opção (1/2/3/4):'))
+num1 = int(input('Digite o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+
+def calculadora(op, n1, n2):
+    if op == 1:
+        print(f'{n1} + {n2} = {n1 + n2}')
+    elif op == 2:
+        print(f'{n1} - {n2} = {n1 - n2}')
+    elif op == 3:
+        print(f'{n1} * {n2} = {n1 * n2}')
+    elif op == 4:
+        print(f'{n1} / {n2} = {n1 / n2}')
+    else:
+        print('Opção inválida!')
+
+
+calculadora(operacao, num1, num2)
+
 
